869a4ff1c51d76e9659223158ffb612212c895dc
hieunm2 <hieunm2@vnext.vn>
2025-03-07 09:45:12 +0700

    unit commit

---
+++ src/main/java/hieu/springbootecommerceultimate/SpringBootEcommerceUltimateApplication.java
@@ -0,0 +1,13 @@
+package hieu.springbootecommerceultimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceUltimateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEcommerceUltimateApplication.class, args);
+    }
+
+}
